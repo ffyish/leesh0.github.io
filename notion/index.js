0810fab5fada44a831919c9cd3050e2c36d2db81
suumokr <notedev.jp@gmail.com>
2022-08-04 14:04:00 +0900

    change workflow

--- notion/index.js
+++ notion/index.js
@@ -1,14 +1,14 @@
-const worker = require('./lib/workers')
+// const worker = require('./lib/workers')
 const fs = require('fs')
 const config = require("./config.json")
 
 
 
-async function main2() {
-    // const result = await notion.checkUpdate()
-    const data = await worker.checkNotionUpdates(repo.owner, repo.repo)
-    fs.writeFileSync('result.json', JSON.stringify(data))
-    console.log('Done')
-}
+// async function main2() {
+//     // const result = await notion.checkUpdate()
+//     const data = await worker.checkNotionUpdates(repo.owner, repo.repo)
+//     fs.writeFileSync('result.json', JSON.stringify(data))
+//     console.log('Done')
+// }
 
 console.log(fs.readdirSync(config.contentPath))
